0aef2fde5a11c09c7545f3868f221562e76ef82d
Chris MacNaughton <chmacnaughton@gmail.com>
2013-07-19 15:14:25 -0400

    messing with unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,12 @@
+worker_processes 6
+
+preload_app true
 # What to do right before exec()-ing the new unicorn binary
 before_exec do |server|
   ENV['BUNDLE_GEMFILE'] = '/var/rails/tor_search/current/Gemfile'
 end
 
+working_directory '/var/rails/tor_search/current'
 
 # What to do before we fork a worker
 before_fork do |server, worker|
@@ -36,10 +40,4 @@ stderr_path '/var/rails/tor_search/shared/log/tor_search.stderr.log'
 # Where stdout gets logged
 stdout_path '/var/rails/tor_search/shared/log/tor_search.stdout.log'
 
-# https://newrelic.com/docs/ruby/ruby-gc-instrumentation
-if GC.respond_to?(:enable_stats)
-  GC.enable_stats
-end
-if defined?(GC::Profiler) and GC::Profiler.respond_to?(:enable)
-  GC::Profiler.enable
-end
+timeout 30
