6cfbccfdbc33c3889a66d2691534e4fb8cd70532
Chris MacNaughton <chmacnaughton@gmail.com>
2013-07-20 11:24:40 -0400

    major overhaul of app

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,19 @@
-set :application, "tor_search"
-set :repository,  "git@bitbucket.org:IceyEC/torsearch.git"
+require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
+require 'new_relic/recipes'
+
+set :stages,                     %w(staging production)
+set :default_stage,              "staging"
+
+set :scm,                        :git
+set :repository,                 "..."
+set :deploy_via,                 :remote_cache
+default_run_options[:pty]        = true
+
+set :application,                "tor_search"
+set :use_sudo,                   false
+set :user,                       "app"
+set :normalize_asset_timestamps, false
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -8,6 +22,15 @@ role :web, "ec2-54-224-36-225.compute-1.amazonaws.com"                          
 role :app, "ec2-54-224-36-225.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
 role :db,  "ec2-54-224-36-225.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
+
+before "deploy",                 "deploy:delayed_job:stop"
+before "deploy:migrations",      "deploy:delayed_job:stop"
+
+after  "deploy:update_code",     "deploy:symlink_shared"
+before "deploy:migrate",         "deploy:web:disable", "deploy:db:backup"
+
+after  "deploy",                                      "newrelic:notice_deployment", "deploy:cleanup", "deploy:delayed_job:restart"
+after  "deploy:migrations",      "deploy:web:enable", "newrelic:notice_deployment", "deploy:cleanup", "deploy:delayed_job:restart"
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:update_code", "deploy:migrate"
@@ -26,30 +49,78 @@ set :keep_releases, 3
 set :user, "ubuntu"
 set :use_sudo, true
 
-
 namespace :deploy do
   task :solr_restart, roles: :app, except: {no_release: true} do
     run "curl http://localhost:8983/solr/admin/cores?wt=json&action=RELOAD&core=collection1"
   end
-  task :start, :roles => :app, :except => { :no_release => true } do
-    run "/etc/init.d/unicorn start"
-  end
-
-  task :stop, :roles => :app, :except => { :no_release => true } do
-    run "/etc/init.d/unicorn stop"
-  end
-
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "/etc/init.d/unicorn restart"
-  end
-
-  # Precompile assets
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+  %w[start stop].each do |command|
+    desc "#{command} unicorn server"
+    task command, :roles => :app, :except => { :no_release => true } do
+      run "#{current_path}/config/server/#{rails_env}/unicorn_init.sh #{command}"
     end
   end
 
+  desc "restart unicorn server"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{current_path}/config/server/#{rails_env}/unicorn_init.sh upgrade"
+  end
+
+
+  desc "Link in the production database.yml and assets"
+  task :symlink_shared do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  namespace :delayed_job do
+    desc "Restart the delayed_job process"
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec script/delayed_job restart" rescue nil
+    end
+    desc "Stop the delayed_job process"
+    task :stop, :roles => :app, :except => { :no_release => true } do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec script/delayed_job stop" rescue nil
+    end
+  end
+
+
+  namespace :db do
+    desc "backup of database before migrations are invoked"
+    task :backup, :roles => :db, :only => { :primary => true } do
+      filename = "#{deploy_to}/shared/db_backup/#{stage}_db.#{Time.now.utc.strftime("%Y-%m-%d_%I:%M")}_before_deploy.gz"
+      text = capture "cat #{deploy_to}/current/config/database.yml"
+      yaml = YAML::load(text)["#{stage}"]
+
+      on_rollback { run "rm #{filename}" }
+      run "mysqldump --single-transaction --quick -u#{yaml['username']} -h#{yaml['host']} -p#{yaml['password']} #{yaml['database']} | gzip -c > #{filename}"
+    end
+  end
+
+
+  namespace :web do
+    desc "Maintenance start"
+    task :disable, :roles => :web do
+      on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+      page = File.read("public/503.html")
+      put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+    end
+
+    desc "Maintenance stop"
+    task :enable, :roles => :web do
+      run "rm #{shared_path}/system/maintenance.html"
+    end
+  end
+
+end
+
+
+namespace :log do
+  desc "A pinch of tail"
+  task :tailf, :roles => :app do
+    run "tail -n 10000 -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
+      puts "#{data}"
+      break if stream == :err
+    end
+  end
 end
 
 # if you're still using the script/reaper helper you will need
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,25 +1,56 @@
-worker_processes 3
+# Unicorn configuration file to be running by unicorn_init.sh with capistrano task
+# read an example configuration before: http://unicorn.bogomips.org/examples/unicorn.conf.rb
+#
+# working_directory, pid, paths - internal Unicorn variables must to setup
+# worker_process 4              - is good enough for serve small production application
+# timeout 30                    - time limit when unresponded workers to restart
+# preload_app true              - the most interesting option that confuse a lot of us,
+#                                 just setup is as true always, it means extra work on
+#                                 deployment scripts to make it correctly
+# BUNDLE_GEMFILE                - make Gemfile accessible with new master
+# before_fork, after_fork       - reconnect to all dependent services: DB, Redis, Sphinx etc.
+#                                 deal with old_pid only if CPU or RAM are limited enough
+#
+# config/server/production/unicorn.rb
 
-preload_app true
-# What to do right before exec()-ing the new unicorn binary
+
+app_path          = "/home/app/public_html/app_production/current"
+
+working_directory "#{app_path}"
+pid               "#{app_path}/tmp/pids/unicorn.pid"
+stderr_path       "#{app_path}/log/unicorn.log"
+stdout_path       "#{app_path}/log/unicorn.log"
+
+listen            "/tmp/unicorn.production.sock"
+worker_processes  4
+timeout           30
+preload_app       true
+
+
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = '/var/rails/tor_search/current/Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "#{app_path}/Gemfile"
 end
-working_directory '/var/rails/tor_search/current'
-# What to do before we fork a worker
+
+
 before_fork do |server, worker|
-    defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  if defined?(Resque)
+    Resque.redis.quit
+  end
+
+  sleep 1
 end
 
-# What to do after we fork a worker
+
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  if defined?(Resque)
+    Resque.redis           = 'localhost:6379'
+  end
 end
-
-# Where stderr gets logged
-stderr_path '/var/rails/tor_search/shared/log/tor_search.stderr.log'
-
-# Where stdout gets logged
-stdout_path '/var/rails/tor_search/shared/log/tor_search.stdout.log'
-
-timeout 30
