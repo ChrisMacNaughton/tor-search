0b33400928627a1381d7b9dd17dc1467eb5c5842
Chris MacNaughton <chmacnaughton@gmail.com>
2013-07-19 20:30:39 -0400

    explicit paths again

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,13 +3,12 @@ worker_processes 3
 preload_app true
 # What to do right before exec()-ing the new unicorn binary
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = 'Gemfile'
+  ENV['BUNDLE_GEMFILE'] = '/var/rails/tor_search/current/Gemfile'
 end
 
 # What to do before we fork a worker
 before_fork do |server, worker|
     defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
-
 end
 
 # What to do after we fork a worker
@@ -18,9 +17,9 @@ after_fork do |server, worker|
 end
 
 # Where stderr gets logged
-stderr_path 'log/tor_search.stderr.log'
+stderr_path '/var/rails/tor_search/shared/log/tor_search.stderr.log'
 
 # Where stdout gets logged
-stdout_path 'log/tor_search.stdout.log'
+stdout_path '/var/rails/tor_search/shared/log/tor_search.stdout.log'
 
 timeout 30
