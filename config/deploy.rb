72b50c4232f9c2a988d35149df963c246743dd9d
Chris MacNaughton <chmacnaughton@gmail.com>
2013-07-25 10:19:08 -0400

    handle asset compilation locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -112,7 +112,21 @@ namespace :deploy do
       run "rm #{shared_path}/system/maintenance.html"
     end
   end
+  namespace :assets do
 
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
 end
 
 
