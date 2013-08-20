830537d5fdabe5e137482bb870cf90a0d0b0a864
Chris MacNaughton <chmacnaughton@gmail.com>
2013-08-20 10:26:48 -0400

    fix broken admin login

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -11,4 +11,15 @@ class Admin < ActiveRecord::Base
   def self.current
     Thread.current[:admin]
   end
+  def username
+    email
+  end
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:username)
+      where(conditions).where(["lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
