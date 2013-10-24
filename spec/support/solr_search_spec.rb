2b03b981210fee6cb8151e3d0da5ee616cdb085c
Chris MacNaughton <chmacnaughton@gmail.com>
2013-10-24 14:55:40 -0400

    add testing to ensure banned domains are not returned in search

--- spec/support/solr_search_spec.rb
+++ spec/support/solr_search_spec.rb
@@ -1,9 +1,25 @@
 describe SolrSearch do
+
   it 'can get the number of indexed docs' do
     VCR.use_cassette('get_solr_index_size') do
       search = SolrSearch.new
 
       search.indexed.should == 134509
+    end
+  end
+
+  it "doesn't return banned domains in the search" do
+    VCR.use_cassette('get_fresh_solr_search') do
+      search = SolrSearch.new('test')
+      host = search.records.first['host']
+      #debugger
+      BannedDomain.create!(hostname: host, reason: "Testing!!!")
+    end
+
+    VCR.use_cassette('get_rejected_solr_search') do
+      host2 = SolrSearch.new('test').records.first['host']
+      host1 = BannedDomain.first.hostname
+      host2.should_not == host1
     end
   end
 end
