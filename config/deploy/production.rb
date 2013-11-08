b9bab28496fc5ff5211ba1f0c05848ae42bb6a64
Chris MacNaughton <chmacnaughton@gmail.com>
2013-11-08 15:19:09 -0500

    change deploy host to go directly

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,12 +3,12 @@
 #
 # config/deploy/production.rb
 
-server 'dlweb02', \
+server 'app@sub.getsy.net:9024', \
       :app, :web, :db, primary: true
 
 #role :db, 'dlweb01'
 
-set :gateway, 'cmacnaughton@sub.gesty.net:9022'
+#set :gateway, 'cmacnaughton@sub.gesty.net:9022'
 set :branch,                     'master'
 set :deploy_to,                  '/var/rails/tor_search'
 set :rails_env,                  'production'
