fb173adfe7940910346311f36e2c2bd3a77c27e3
Chris MacNaughton <chmacnaughton@gmail.com>
2013-11-26 11:23:21 -0500

    change callback url

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -132,7 +132,7 @@ class AdsController < ApplicationController
       coinbase = Coinbase::Client.new(TorSearch::Application.config.tor_search.coinbase_key)
       options = {
         address: {
-          callback_url: 'http://ts.chrismacnaughton.com:8080/payments'
+          callback_url: 'https://torsearch.es/payments'
         }
       }
       address = coinbase.generate_receive_address(options)
