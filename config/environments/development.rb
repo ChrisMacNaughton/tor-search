ee29ab2f3a7c32ba3c467833f1e4987974a1d0e9
Chris MacNaughton <chmacnaughton@gmail.com>
2014-01-07 10:50:14 -0500

    have cache path when needed in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ TorSearch::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  #config.cache_store = :file_store, "/Users/chris/ts/tor_search/tmp/testing_cache"
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
