542ec2ff7b30e7a5f9aebe58ba4f86cd97d40684
Chris MacNaughton <chmacnaughton@gmail.com>
2014-01-17 08:08:09 -0500

    redirect to root when missing a param
    [finished #64041730]

--- app/controllers/instant_controller.rb
+++ app/controllers/instant_controller.rb
@@ -3,11 +3,15 @@ require "#{Rails.root}/lib/matcher/matcher"
 
 class InstantController < ApplicationController
   def new
-    term = params[:search]
+    if params[:search]
+      term = params[:search]
 
-    matches = Matcher.new(term, request).execute || []
-    hash = { meta: { searched: term }, matches: matches }
+      matches = Matcher.new(term, request).execute || []
+      hash = { meta: { searched: term }, matches: matches }
 
-    render json: hash
+      render json: hash
+    else
+      redirect_to root_path
+    end
   end
 end
