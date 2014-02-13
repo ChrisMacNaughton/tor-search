efd665b737dfa7fc548b99102f153273474b4f9f
Chris MacNaughton <chmacnaughton@gmail.com>
2014-02-13 16:18:21 -0500

    add a has_many from ad_campaigns to ads

--- app/models/ad_campaign.rb
+++ app/models/ad_campaign.rb
@@ -2,4 +2,5 @@ class AdCampaign < ActiveRecord::Base
   attr_accessible :name, :status, :paused, :advertiser, :advertiser_id, :default_bid
   belongs_to :advertiser
   has_many :ad_groups, dependent: :destroy
+  has_many :ads, through: :ad_groups
 end
