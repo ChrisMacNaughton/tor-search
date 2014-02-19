6c21d1abc9aed1c0729af33777990eb5a33b163b
Chris MacNaughton <chmacnaughton@gmail.com>
2014-02-19 12:16:01 -0500

    add keyword search history to keywords page
    [finished #65096552]

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 # a keyword that can match a search
 class Keyword < ActiveRecord::Base
-  attr_accessible :word
+  attr_accessible :word, :searches_counts, :status_id
+
+  STATUS_IDS = [0,1,2]
+  STATUS_NEGATIVE = 0
+  STATUS_STATIC = 1
+  STATUS_POSITIVE = 2
+
 end
