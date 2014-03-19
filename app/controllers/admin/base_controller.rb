58435e19a65268550668b41a9c5c6fe208e5f77c
Chris MacNaughton <chmacnaughton@gmail.com>
2014-03-19 08:47:21 -0400

    large cleanup

--- app/controllers/admin/base_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::BaseController < ActionController::Base
-  include CacheSupport
-  include TorMethods
-  force_ssl unless Proc.new { ssl_configured?  && is_tor? }
-
-  before_filter :authenticate_admin!
-  layout 'admin'
-end
