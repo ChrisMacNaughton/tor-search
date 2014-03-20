0bda9c6017c35e4b22fb4539a38d1a5b2d18daae
Chris MacNaughton <chmacnaughton@gmail.com>
2014-03-20 08:07:58 -0400

    updating deploy / error pages

--- config/deploy.rb
+++ config/deploy.rb
@@ -107,21 +107,6 @@ namespace :deploy do
     end
   end
 
-  namespace :assets do
-    task :precompile, roles: :web do
-      from = source.next_revision(current_revision) rescue nil
-      if from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-        run_locally('rake assets:clean && rake assets:precompile')
-        run_locally 'cd public && tar -jcf assets.tar.bz2 assets'
-        top.upload 'public/assets.tar.bz2', "#{shared_path}", :via => :scp
-        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-        run_locally 'rm public/assets.tar.bz2'
-        run_locally('rake assets:clean')
-      else
-        logger.info 'Skipping asset precompilation because there were no asset changes'
-      end
-    end
-  end
 end
 
 namespace :logs do
