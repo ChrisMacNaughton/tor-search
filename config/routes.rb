29056e0569b776ebf79f939c24e1e36fd5fb768d
Chris MacNaughton <chmacnaugton@gmail.com>
2014-06-26 11:03:44 -0400

    just change to post

--- config/routes.rb
+++ config/routes.rb
@@ -53,8 +53,8 @@ TorSearch::Application.routes.draw do
       resources :keywords, controller: :ad_group_keywords
     end
     resources :ads do
-      put 'delete' => 'ads#delete', as: :delete
-      put 'restore' => 'ads#restore', as: :restore
+      post 'delete' => 'ads#delete', as: :delete
+      post 'restore' => 'ads#restore', as: :restore
       get 'toggle' => 'ads#toggle', as: :toggle
       # put 'toggle' => 'ads#toggle', as: :toggle
     end
